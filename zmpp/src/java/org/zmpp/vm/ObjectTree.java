482cd7c52407f956591d3c12bb68db39a57a1d20
weiju <weiju@7df28f57-6730-0410-8a28-a3245024edd1>
2005-10-14 18:06:07 +0000

    Defined interfaces for ZObject and ObjectTree for easier testing
    git-svn-id: https://zmpp.svn.sourceforge.net/svnroot/zmpp/trunk@52 7df28f57-6730-0410-8a28-a3245024edd1

---
+++ zmpp/src/java/org/zmpp/vm/ObjectTree.java
@@ -0,0 +1,65 @@
+/*
+ * $Id$
+ * 
+ * Created on 14.10.2005
+ * Copyright 2005 by Wei-ju Wu
+ *
+ * This file is part of The Z-machine Preservation Project (ZMPP).
+ *
+ * ZMPP is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * ZMPP is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with ZMPP; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+package org.zmpp.vm;
+
+/**
+ * This is the interface definition of the object tree.
+ * 
+ * @author Wei-ju Wu
+ * @version 1.0
+ */
+public interface ObjectTree {
+
+  /**
+   * Returns the property default value at the specified position in the
+   * property defaults table.
+   * 
+   * @param index the default entry's index
+   * @return the property default value at index <i>index</i>
+   */
+  int getPropertyDefault(int propertyNum);
+  
+  /**
+   * Returns the Z-object with the specified object number. The index is
+   * 1 based
+   * 
+   * @param objectNum object number
+   * @return the z object with the specified number
+   */
+  ZObject getObject(int objectNum);  
+  
+  /**
+   * Removes an object from its parent.
+   * 
+   * @param objectNum the object number
+   */
+  void removeObject(int objectNum);
+    
+  /**
+   * Inserts an object to a new parent.
+   * 
+   * @param parentNum the parent number
+   * @param objectNum the object number
+   */
+  void insertObject(int parentObj, int objectNum);
+}
--- zmpp/src/test/test/zmpp/vm/ObjectsTest.java
+++ zmpp/src/test/test/zmpp/vm/ObjectsTest.java
@@ -23,7 +23,7 @@
 package test.zmpp.vm;
 
 import org.zmpp.vm.Objects;
-import org.zmpp.vm.Objects.Zobject;
+import org.zmpp.vm.ZObject;
 
 public class ObjectsTest extends MemoryMapSetup {
 
@@ -43,53 +43,53 @@ public class ObjectsTest extends MemoryMapSetup {
   
   public void testGetObjectAt() {
     
-    assertEquals(36, objects.getObjectAt(1).getParent());
-    assertEquals(147, objects.getObjectAt(1).getSibling());
-    assertEquals(0, objects.getObjectAt(1).getChild());
-    assertEquals(0x0a4f, objects.getObjectAt(1).getPropertyTableAddress());
-    assertTrue(objects.getObjectAt(1).isAttributeSet(6));
+    assertEquals(36, objects.getObject(1).getParent());
+    assertEquals(147, objects.getObject(1).getSibling());
+    assertEquals(0, objects.getObject(1).getChild());
+    assertEquals(0x0a4f, objects.getObject(1).getPropertyTableAddress());
+    assertTrue(objects.getObject(1).isAttributeSet(6));
     
-    assertEquals(27, objects.getObjectAt(2).getParent());
-    assertEquals(119, objects.getObjectAt(2).getSibling());
-    assertEquals(95, objects.getObjectAt(2).getChild());
-    assertEquals(0x0a5d, objects.getObjectAt(2).getPropertyTableAddress());    
-    assertTrue(objects.getObjectAt(2).isAttributeSet(5));
-    assertFalse(objects.getObjectAt(2).isAttributeSet(6));
-    assertTrue(objects.getObjectAt(2).isAttributeSet(7));
-    assertTrue(objects.getObjectAt(2).isAttributeSet(19));
+    assertEquals(27, objects.getObject(2).getParent());
+    assertEquals(119, objects.getObject(2).getSibling());
+    assertEquals(95, objects.getObject(2).getChild());
+    assertEquals(0x0a5d, objects.getObject(2).getPropertyTableAddress());    
+    assertTrue(objects.getObject(2).isAttributeSet(5));
+    assertFalse(objects.getObject(2).isAttributeSet(6));
+    assertTrue(objects.getObject(2).isAttributeSet(7));
+    assertTrue(objects.getObject(2).isAttributeSet(19));
     
-    assertFalse(objects.getObjectAt(27).isAttributeSet(0));
-    assertFalse(objects.getObjectAt(27).isAttributeSet(1));    
+    assertFalse(objects.getObject(27).isAttributeSet(0));
+    assertFalse(objects.getObject(27).isAttributeSet(1));    
   }
   
   public void testSetAttributes() {
     
-    assertFalse(objects.getObjectAt(1).isAttributeSet(5));
-    objects.getObjectAt(1).setAttribute(5);
-    assertTrue(objects.getObjectAt(1).isAttributeSet(5));
+    assertFalse(objects.getObject(1).isAttributeSet(5));
+    objects.getObject(1).setAttribute(5);
+    assertTrue(objects.getObject(1).isAttributeSet(5));
   }
   
   public void testClearAttributes() {
     
     // Set several bits in a row to make sure there will be no arithmetical
     // shift errors
-    objects.getObjectAt(1).setAttribute(0);
-    objects.getObjectAt(1).setAttribute(1);
-    objects.getObjectAt(1).setAttribute(2);
-    objects.getObjectAt(1).setAttribute(3);
+    objects.getObject(1).setAttribute(0);
+    objects.getObject(1).setAttribute(1);
+    objects.getObject(1).setAttribute(2);
+    objects.getObject(1).setAttribute(3);
     
-    assertTrue(objects.getObjectAt(1).isAttributeSet(2));
-    objects.getObjectAt(1).clearAttribute(2);
+    assertTrue(objects.getObject(1).isAttributeSet(2));
+    objects.getObject(1).clearAttribute(2);
     
-    assertTrue(objects.getObjectAt(1).isAttributeSet(0));
-    assertTrue(objects.getObjectAt(1).isAttributeSet(1));
-    assertFalse(objects.getObjectAt(1).isAttributeSet(2));
-    assertTrue(objects.getObjectAt(1).isAttributeSet(3));    
+    assertTrue(objects.getObject(1).isAttributeSet(0));
+    assertTrue(objects.getObject(1).isAttributeSet(1));
+    assertFalse(objects.getObject(1).isAttributeSet(2));
+    assertTrue(objects.getObject(1).isAttributeSet(3));    
   }
   
   public void testObjectSetters() {
     
-    Zobject obj = objects.getObjectAt(1);
+    ZObject obj = objects.getObject(1);
     obj.setParent((short) 38);
     assertEquals(38, obj.getParent());
     obj.setChild((short) 39);
@@ -105,36 +105,36 @@ public class ObjectsTest extends MemoryMapSetup {
   
   public void testGetPropertiesTable() {
     
-    int propaddress = objects.getObjectAt(1).getPropertiesDescriptionAddress();
+    int propaddress = objects.getObject(1).getPropertiesDescriptionAddress();
     assertEquals("forest", converter.convert(minizorkmap, propaddress));
-    assertEquals(2, objects.getObjectAt(1).getNumProperties());
-    assertEquals(2, objects.getObjectAt(1).getPropertySize(18));
-    assertEquals(4, objects.getObjectAt(1).getPropertySize(17));
-    assertEquals(2644, objects.getObjectAt(1).getPropertyAddress(18));
-    assertEquals(2647, objects.getObjectAt(1).getPropertyAddress(17));
-    assertTrue(objects.getObjectAt(1).isPropertyAvailable(18));
-    assertTrue(objects.getObjectAt(1).isPropertyAvailable(17));
-    assertFalse(objects.getObjectAt(1).isPropertyAvailable(5));
+    assertEquals(2, objects.getObject(1).getNumProperties());
+    assertEquals(2, objects.getObject(1).getPropertySize(18));
+    assertEquals(4, objects.getObject(1).getPropertySize(17));
+    assertEquals(2644, objects.getObject(1).getPropertyAddress(18));
+    assertEquals(2647, objects.getObject(1).getPropertyAddress(17));
+    assertTrue(objects.getObject(1).isPropertyAvailable(18));
+    assertTrue(objects.getObject(1).isPropertyAvailable(17));
+    assertFalse(objects.getObject(1).isPropertyAvailable(5));
     
-    assertEquals(0x43, objects.getObjectAt(1).getPropertyByte(18, 0));
-    assertEquals(0xa7, objects.getObjectAt(1).getPropertyByte(18, 1));
+    assertEquals(0x43, objects.getObject(1).getPropertyByte(18, 0));
+    assertEquals(0xa7, objects.getObject(1).getPropertyByte(18, 1));
 
-    assertEquals(0x2d, objects.getObjectAt(1).getPropertyByte(17, 0));
-    assertEquals(0x23, objects.getObjectAt(1).getPropertyByte(17, 1));
-    assertEquals(0x35, objects.getObjectAt(1).getPropertyByte(17, 2));
-    assertEquals(0x8f, objects.getObjectAt(1).getPropertyByte(17, 3));    
+    assertEquals(0x2d, objects.getObject(1).getPropertyByte(17, 0));
+    assertEquals(0x23, objects.getObject(1).getPropertyByte(17, 1));
+    assertEquals(0x35, objects.getObject(1).getPropertyByte(17, 2));
+    assertEquals(0x8f, objects.getObject(1).getPropertyByte(17, 3));    
   }
   
   public void testGetNextProperty() {
     
-    assertEquals(18, objects.getObjectAt(1).getNextProperty(0));
-    assertEquals(17, objects.getObjectAt(1).getNextProperty(18));
-    assertEquals(0, objects.getObjectAt(1).getNextProperty(17));
+    assertEquals(18, objects.getObject(1).getNextProperty(0));
+    assertEquals(17, objects.getObject(1).getNextProperty(18));
+    assertEquals(0, objects.getObject(1).getNextProperty(17));
   }
   
   public void testSetPropertyByte() {
     
-    objects.getObjectAt(1).setPropertyByte(18, 0, (short) 0x12);
-    assertEquals(0x12, objects.getObjectAt(1).getPropertyByte(18, 0));
+    objects.getObject(1).setPropertyByte(18, 0, (short) 0x12);
+    assertEquals(0x12, objects.getObject(1).getPropertyByte(18, 0));
   }
 }
