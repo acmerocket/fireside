6fb78e74a768ddcb596c8b084ebe802d8f77914d
weiju <weiju@7df28f57-6730-0410-8a28-a3245024edd1>
2005-10-20 05:25:39 +0000

    Outfactored Screen3
    git-svn-id: https://zmpp.svn.sourceforge.net/svnroot/zmpp/trunk@59 7df28f57-6730-0410-8a28-a3245024edd1

--- zmpp-swing/src/java/org/zmpp/swingui/Main.java
+++ zmpp-swing/src/java/org/zmpp/swingui/Main.java
@@ -1,7 +1,7 @@
 /*
  * $Id$
  * 
- * Created on 24.09.2005
+ * Created on 17.10.2005
  * Copyright 2005 by Wei-ju Wu
  *
  * This file is part of The Z-machine Preservation Project (ZMPP).
@@ -22,29 +22,16 @@
  */
 package org.zmpp.swingui;
 
-import java.awt.BorderLayout;
-import java.awt.FlowLayout;
-import java.awt.GridLayout;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.io.File;
 import java.io.IOException;
 import java.io.RandomAccessFile;
 
 import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JMenu;
-import javax.swing.JMenuBar;
-import javax.swing.JMenuItem;
-import javax.swing.JPanel;
-import javax.swing.JScrollPane;
-import javax.swing.JViewport;
 
 import org.zmpp.base.MemoryAccess;
 import org.zmpp.vm.Abbreviations;
 import org.zmpp.vm.Machine3;
 import org.zmpp.vm.StoryFileHeader;
-import org.zmpp.vm.ZObject;
 import org.zmpp.vmutil.ZsciiConverter;
 import org.zmpp.vmutil.ZsciiString;
 
@@ -54,57 +41,8 @@ import org.zmpp.vmutil.ZsciiString;
  * @author Wei-ju Wu
  * @version 1.0
  */
-public class Main extends JFrame {
+public class Main {
 
-  /**
-   * Serial version UID.
-   */
-  private static final long serialVersionUID = 1L;
-  
-  private JLabel global1ObjectLabel;
-  private JLabel statusLabel;
-  
-  public Main(Machine3 machine) {
-    
-    super("Z-Machine Preservation Project Version 1.0 (alpha)");
-    
-    setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    JViewport viewport = new JViewport();
-    JScrollPane spane = new JScrollPane(viewport);
-    JPanel statusPanel = new JPanel(new GridLayout(1, 2));
-    JPanel status1Panel = new JPanel(new FlowLayout(FlowLayout.LEFT));
-    JPanel status2Panel = new JPanel(new FlowLayout(FlowLayout.RIGHT));
-    statusPanel.add(status1Panel);
-    statusPanel.add(status2Panel);
-    
-    int objNum = machine.getVariable(0x10);    
-    ZObject obj = machine.getObjectTree().getObject(objNum);
-    String objectName = (new ZsciiString(machine.getMemoryAccess(),
-          obj.getPropertiesDescriptionAddress())).toString();
-    
-    global1ObjectLabel = new JLabel("West of House");
-    statusLabel = new JLabel("0/0");
-    status1Panel.add(global1ObjectLabel);
-    status2Panel.add(statusLabel);
-    
-    getContentPane().add(statusPanel, BorderLayout.NORTH);
-    getContentPane().add(spane, BorderLayout.CENTER);
-    
-    JMenuBar menubar = new JMenuBar();
-    setJMenuBar(menubar);
-    JMenu fileMenu = new JMenu("File");
-    fileMenu.setMnemonic('F');
-    menubar.add(fileMenu);
-    JMenuItem exitItem = new JMenuItem("Exit");
-    exitItem.setMnemonic('x');
-    fileMenu.add(exitItem);
-    exitItem.addActionListener(new ActionListener() {
-      public void actionPerformed(ActionEvent e) {
-        
-        System.exit(0);
-      }
-    });
-  }
   
   /**
    * @param args
@@ -131,7 +69,7 @@ public class Main extends JFrame {
       Machine3 machineState = new Machine3();
       machineState.initialize(memaccess, fileheader);
       
-      JFrame frame = new Main(machineState);
+      JFrame frame = new Screen3(machineState);
       frame.pack();
       frame.setVisible(true);
             
