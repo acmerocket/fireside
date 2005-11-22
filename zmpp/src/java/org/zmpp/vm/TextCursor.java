26a00a12090c23954f0de6b16592e6d3b408baa2
weiju <weiju@7df28f57-6730-0410-8a28-a3245024edd1>
2005-11-22 22:38:18 +0000

    initial version
    git-svn-id: https://zmpp.svn.sourceforge.net/svnroot/zmpp/trunk@147 7df28f57-6730-0410-8a28-a3245024edd1

---
+++ zmpp/src/java/org/zmpp/vm/TextCursor.java
@@ -0,0 +1,30 @@
+/*
+ * $Id$
+ * 
+ * Created on 11/22/2005
+ * Copyright 2005 by Wei-ju Wu
+ *
+ * This file is part of The Z-machine Preservation Project (ZMPP).
+ *
+ * ZMPP is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * ZMPP is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with ZMPP; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+package org.zmpp.vm;
+
+public interface TextCursor {
+
+  int getLine();
+  int getColumn();
+  void setPosition(int line, int column);
+}
