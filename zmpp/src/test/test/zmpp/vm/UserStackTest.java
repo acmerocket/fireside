2c486f7dab2dff1e73ca8a7f85203c60046a03bb
weiju <weiju@7df28f57-6730-0410-8a28-a3245024edd1>
2006-05-12 21:45:37 +0000

    initial version
    git-svn-id: https://zmpp.svn.sourceforge.net/svnroot/zmpp/trunk@470 7df28f57-6730-0410-8a28-a3245024edd1

---
+++ zmpp/src/test/test/zmpp/vm/UserStackTest.java
@@ -0,0 +1,101 @@
+/*
+ * $Id$
+ * 
+ * Created on 2006/05/11
+ * Copyright 2005-2006 by Wei-ju Wu
+ *
+ * This file is part of The Z-machine Preservation Project (ZMPP).
+ *
+ * ZMPP is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * ZMPP is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with ZMPP; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+package test.zmpp.vm;
+
+import org.jmock.Mock;
+import org.jmock.MockObjectTestCase;
+import org.zmpp.base.DefaultMemoryAccess;
+import org.zmpp.base.MemoryAccess;
+import org.zmpp.vm.Cpu;
+import org.zmpp.vm.CpuImpl;
+import org.zmpp.vm.GameData;
+import org.zmpp.vm.InstructionDecoder;
+import org.zmpp.vm.Machine;
+import org.zmpp.vm.StoryFileHeader;
+
+/**
+ * This class tests the access method to the user stack.
+ * 
+ * @author Wei-ju Wu
+ * @version 1.0
+ */
+public class UserStackTest extends MockObjectTestCase {
+
+  private Mock mockMachine, mockDecoder, mockGameData, mockFileHeader;
+  private Machine machine;
+  private InstructionDecoder decoder;
+  private GameData gamedata;
+  private Cpu cpu;
+  private StoryFileHeader fileheader;
+  private MemoryAccess memaccess;
+  
+  // A stack with three words, but only two slots
+  private byte[] stackdata = {  0x00, 0x02, 0x00, 0x00, 0x00, 0x00 };
+  
+  public void setUp() throws Exception {
+    
+
+    mockMachine = mock(Machine.class);
+    mockDecoder = mock(InstructionDecoder.class);
+    mockGameData = mock(GameData.class);
+    mockFileHeader = mock(StoryFileHeader.class);
+    
+    machine = (Machine) mockMachine.proxy();
+    decoder = (InstructionDecoder) mockDecoder.proxy();
+    gamedata = (GameData) mockGameData.proxy();
+    fileheader = (StoryFileHeader) mockFileHeader.proxy();
+    memaccess = new DefaultMemoryAccess(stackdata);
+    
+    mockMachine.expects(atLeastOnce()).method("getGameData").will(returnValue(gamedata));
+    mockGameData.expects(atLeastOnce()).method("getStoryFileHeader").will(returnValue(fileheader));
+    mockFileHeader.expects(once()).method("getProgramStart").will(returnValue(1000));
+    mockFileHeader.expects(once()).method("getGlobalsAddress").will(returnValue(5000));
+    mockFileHeader.expects(once()).method("getVersion").will(returnValue(5));
+    mockGameData.expects(atLeastOnce()).method("getMemoryAccess").will(returnValue(memaccess));
+    mockDecoder.expects(once()).method("initialize").with(eq(machine), eq(memaccess));
+    cpu = new CpuImpl(machine, decoder);
+    cpu.reset();
+  }
+  
+  public void testPushStack() {
+    
+    assertTrue(cpu.pushUserStack(0, (short) 12));
+    assertEquals("stack should contain only one slot now", 1, memaccess.readUnsignedShort(0));
+    assertEquals("value 12 should be in the second slot", 12, memaccess.readShort(4));
+  }
+
+  public void testPushStackOverflowError() {
+
+    assertTrue(cpu.pushUserStack(0, (short) 12));
+    assertTrue(cpu.pushUserStack(0, (short) 12));
+    assertFalse(cpu.pushUserStack(0, (short) 12));
+  }
+  
+  public void testPopStack() {
+    
+    cpu.pushUserStack(0, (short) 12);
+    cpu.pushUserStack(0, (short) 13);
+    assertEquals("first word on stack should be 13", 13, cpu.popUserStack(0));
+    assertEquals("second word on stack should be 12", 12, cpu.popUserStack(0));    
+  }
+}
---
+++ zmpp/src/test/test/zmpp/vmutil/FastShortStackTest.java
@@ -0,0 +1,91 @@
+/*
+ * $Id$
+ * 
+ * Created on 2006/05/10
+ * Copyright 2005-2006 by Wei-ju Wu
+ *
+ * This file is part of The Z-machine Preservation Project (ZMPP).
+ *
+ * ZMPP is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * ZMPP is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with ZMPP; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+package test.zmpp.vmutil;
+
+import junit.framework.TestCase;
+
+import org.zmpp.vmutil.FastShortStack;
+
+public class FastShortStackTest extends TestCase {
+
+  private FastShortStack stack;
+  
+  protected void setUp() throws Exception {
+
+    this.stack = new FastShortStack(123);
+  }
+
+  public void testInitial() {
+    
+    assertEquals(0, stack.getStackPointer());
+    assertEquals(0, stack.size());
+  }
+  
+  public void testSize() {
+    
+    stack.push((short) 1);
+    assertEquals(1, stack.size());
+    stack.push((short) 3);
+    assertEquals(2, stack.size());
+    stack.pop();
+    assertEquals(1, stack.size());
+  }
+  
+  public void testPushTop() {
+    
+    stack.push((short) 3);
+    assertEquals("stack pointer should have been increased", 1, stack.getStackPointer());
+    assertEquals("the value 3 should be on top of the stack", 3, stack.top());
+    assertEquals("stack pointer should not have been modified", 1, stack.getStackPointer());    
+  }
+  
+  public void testPushPop() {
+    
+    stack.push((short) 3);
+    assertEquals("the value 3 should be on top of the stack", 3, stack.pop());
+    assertEquals("stack pointer should have been decreased", 0, stack.getStackPointer());
+  }
+    
+  public void testGetValueAt() {
+    
+    stack.push((short) 3);
+    stack.push((short) 5);
+    stack.push((short) 7);
+    
+    assertEquals(3, stack.getValueAt(0));
+    assertEquals(5, stack.getValueAt(1));
+    assertEquals(7, stack.getValueAt(2));
+    assertEquals("stack pointer should not have been modified", 3, stack.getStackPointer());
+  }
+  
+  public void testReplaceTopElement() {
+    
+    stack.push((short) 3);
+    stack.push((short) 5);
+    stack.push((short) 7);
+    stack.replaceTopElement((short) 11);
+    assertEquals("top element should be 11 now", 11, stack.top());
+    assertEquals("number of elements should be 3", 3, stack.size());
+  }
+  
+}
